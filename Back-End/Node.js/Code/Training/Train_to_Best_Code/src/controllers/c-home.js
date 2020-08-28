4bda7199df378eb77b249f6d57829a6407475e4d
kimmincheol-kor <kimmincheol.dev@gmail.com>
2020-08-28 16:51:49 +0900

    Update TTBC : Router-Controller Structure

---
+++ Back-End/Node.js/Code/Training/Train_to_Best_Code/src/controllers/c-home.js
@@ -0,0 +1,15 @@
+exports.getHome = (req, res) => {
+    res.send('GET home');
+}
+
+exports.postHome = (req, res) => {
+    res.send('POST home');
+}
+
+exports.putHome = (req, res) => {
+    res.send('PUT home');
+}
+
+exports.deleteHome = (req, res) => {
+    res.send('DELETE home');
+}
--- Back-End/Node.js/Code/Training/Train_to_Best_Code/src/routes/home.js
+++ Back-End/Node.js/Code/Training/Train_to_Best_Code/src/routes/home.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/c-home');
 
-router.get('/', (req, res) => {
-    res.send('hi get');
-});
+// Routes - Controllers Structure
+router.route('/')
+    .get(controller.getHome)
+    .post(controller.postHome)
+    .put(controller.putHome)
+    .delete(controller.deleteHome)
 
-router.post('/', (req, res) => {
-    res.send('hi post');
-});
 
-router.put('/', (req, res) => {
-    res.send('hi put');
-});
 
-router.delete('/', (req, res) => {
-    res.send('hi delete');
-});
+// router.get('/', (req, res) => {
+//     res.send('hi get');
+// });
+
+// router.post('/', (req, res) => {
+//     res.send('hi post');
+// });
+
+// router.put('/', (req, res) => {
+//     res.send('hi put');
+// });
+
+// router.delete('/', (req, res) => {
+//     res.send('hi delete');
+// });
 
 module.exports = router;
