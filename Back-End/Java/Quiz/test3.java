8c204104148a0c4b8a265301d25a507dc1d67747
kimmincheol-kor <kimmincheol.dev@gmail.com>
2020-11-16 15:59:40 +0900

    Upload Quiz Code : Test3 (LCS string)

--- Back-End/Java/Quiz/test3.java
+++ Back-End/Java/Quiz/test3.java
@@ -1,29 +1,30 @@
+public class test3 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution("abcdcba"));
+    }
+}
+
 class Solution
 {
     public int solution(String s)
     {
-        int checkLength = s.length();
-
-        for ( ; checkLength > 1; checkLength--) {
-            int halfLength = checkLength / 2;
-
-            for(int i=0; i+halfLength*2<s.length(); i++) {
-                String a = s.substring(i, i+checkLength);
-                if (checker(a)) {
-                    return checkLength;
+        int answer = 1;
+        int[][] dp = new int[s.length()+1][s.length()+1];
+        String reverse_s = (new StringBuffer(s)).reverse().toString();
+        
+        // LCS s - reverse_s
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j <= s.length(); j++) {
+                dp[i][j] = 0;
+                if ( i>0 && j>0 && s.charAt(i-1) == reverse_s.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    if (dp[i][j] > answer) answer = dp[i][j];
                 }
             }
         }
-
-        return 1;
-    }
-
-    public boolean checker(String a) {
-        int len = a.length();
-
-        for(int i=0; i<len/2; i++) {
-            if (a.charAt(i) != a.charAt(len-1-i)) return false;
-        }
-        return true;
+    
+        return answer;
     }
 }
