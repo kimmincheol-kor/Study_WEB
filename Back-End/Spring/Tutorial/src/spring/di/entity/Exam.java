9dc0ababe03d7930835a421fe8f96b905024cffc
Mincheol Kim <62646365+kimmincheol-kor@users.noreply.github.com>
2021-04-19 23:32:25 +0900

    Delete Back-End/Spring directory

--- Back-End/Spring/Tutorial/src/spring/di/entity/Exam.java
+++
@@ -1,6 +0,0 @@
-package spring.di.entity;
-
-public interface Exam {
-	int total();
-	float avg();
-}
--- Back-End/Spring/Tutorial/src/spring/di/ui/ExamConsole.java
+++
@@ -1,8 +0,0 @@
-package spring.di.ui;
-
-import spring.di.entity.Exam;
-
-public interface ExamConsole {
-	void print();
-	void setExam(Exam exam);
-}
